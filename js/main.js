0859f681b91241afccfc65c65182ce4023c9c9d2
Nicolas Martinez <nicolasfmartinez@hotmail.com>
2021-10-24 18:03:29 -0300

    ingreso de Dom creacion de un div

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-let nombreUsuario = prompt("Por favor ingrese su nombre");
 
-
-
-alert("Bienvenid@ a Limo Tienda " + nombreUsuario+ " !"); //mensaje de bienvenida
 const catalogo = [] //creacion del ArrayProductos
 
 
 
 class producto {
-    constructor(genero, nombre, precio, stock) {
+    constructor(id, genero, nombre, precio, stock) {
+        this.id=id;
         this.genero = genero;
         this.nombre = nombre;
         this.precio = precio;
@@ -17,48 +14,48 @@ class producto {
 }
 
 
-catalogo.push(new producto("Buzo", "Growing", 3200, 5));
-catalogo.push(new producto("Buzo", "Growing2", 3200, 6));
-catalogo.push(new producto("Buzo", "Save", 3000, 2));
-catalogo.push(new producto("Buzo", "Fungi", 3200, 4));
-catalogo.push(new producto("Buzo", "Atenas", 3600, 4));
-catalogo.push(new producto("Lentes", "Boston", 1500, 3));
-catalogo.push(new producto("Lentes", "Balvin", 1400, 3));
-catalogo.push(new producto("Lentes", "Cardy", 1700, 2));
-catalogo.push(new producto("Lentes", "Kendal", 1300, 6));
-catalogo.push(new producto("Lentes", "Kurt", 1800, 5));
-catalogo.push(new producto("Lentes", "Nass", 1100, 7));
-catalogo.push(new producto("Lentes", "Nasty", 1700, 2));
-catalogo.push(new producto("Lentes", "Queer", 1700, 2));
-catalogo.push(new producto("Lentes", "Shilow", 1700, 2));
-catalogo.push(new producto("Lentes", "Tao", 1700, 2));
-catalogo.push(new producto("Lentes", "Trish", 1700, 2));
-catalogo.push(new producto("Medias", "Alien", 400, 6));
-catalogo.push(new producto("Medias", "Drake", 390, 3));
-catalogo.push(new producto("Medias", "Fire", 500, 2));
-catalogo.push(new producto("Medias", "Frute", 390, 4));
-catalogo.push(new producto("Medias", "Fuck", 400, 2));
-catalogo.push(new producto("Medias", "PrideB", 450, 6));
-catalogo.push(new producto("Medias", "PrideW", 400, 2));
-catalogo.push(new producto("Medias", "Sativa", 425, 3));
-catalogo.push(new producto("Medias", "Ska", 390, 2));
-catalogo.push(new producto("Gorro", "Good", 650, 2));
-catalogo.push(new producto("Gorro", "Miche", 920, 2));
-catalogo.push(new producto("Gorro", "Rainbow", 900, 2));
-catalogo.push(new producto("Remera", "NoProblem", 1200, 2));
-catalogo.push(new producto("Remera", "FungiR", 1900, 2));
-catalogo.push(new producto("Remera", "BatikSilver", 1850, 2));
-catalogo.push(new producto("Remera", "BatikColor", 1800, 2));
-catalogo.push(new producto("Accesorio", "ArosChapa", 350, 7));
-catalogo.push(new producto("Accesorio", "CollarMari", 700, 5));
-catalogo.push(new producto("Accesorio", "CollarPant", 600, 4));
-catalogo.push(new producto("Piluso", "Beanie", 800, 2));
-catalogo.push(new producto("Piluso", "PilusoNegro", 600, 2));
-catalogo.push(new producto("Piluso", "PilusoNeon", 600, 2));
-catalogo.push(new producto("Piluso", "PilusoBatik", 750, 7));
-catalogo.push(new producto("Piluso", "PilusoNaranja", 600, 5));
-catalogo.push(new producto("Piluso", "PilusoBlanco", 590, 4));
-
+catalogo.push(new producto(01,"Buzo", "Growing", 3200, 5));
+catalogo.push(new producto(02,"Buzo", "Growing2", 3200, 6));
+catalogo.push(new producto(03,"Buzo", "Save", 3000, 2));
+catalogo.push(new producto(04,"Buzo", "Fungi", 3200, 4));
+catalogo.push(new producto(05,"Buzo", "Atenas", 3600, 4));
+catalogo.push(new producto(06,"Lentes", "Boston", 1500, 3));
+catalogo.push(new producto(07,"Lentes", "Balvin", 1400, 3));
+catalogo.push(new producto(08,"Lentes", "Cardy", 1700, 2));
+catalogo.push(new producto(09,"Lentes", "Kendal", 1300, 6));
+catalogo.push(new producto(10,"Lentes", "Kurt", 1800, 5));
+catalogo.push(new producto(11,"Lentes", "Nass", 1100, 7));
+catalogo.push(new producto(12,"Lentes", "Nasty", 1700, 2));
+catalogo.push(new producto(13,"Lentes", "Queer", 1700, 2));
+catalogo.push(new producto(14,"Lentes", "Shilow", 1700, 2));
+catalogo.push(new producto(15,"Lentes", "Tao", 1700, 2));
+catalogo.push(new producto(16,"Lentes", "Trish", 1700, 2));
+catalogo.push(new producto(17,"Medias", "Alien", 400, 6));
+catalogo.push(new producto(18,"Medias", "Drake", 390, 3));
+catalogo.push(new producto(19,"Medias", "Fire", 500, 2));
+catalogo.push(new producto(20,"Medias", "Frute", 390, 4));
+catalogo.push(new producto(21,"Medias", "Fuck", 400, 2));
+catalogo.push(new producto(22,"Medias", "PrideB", 450, 6));
+catalogo.push(new producto(23,"Medias", "PrideW", 400, 2));
+catalogo.push(new producto(24,"Medias", "Sativa", 425, 3));
+catalogo.push(new producto(25,"Medias", "Ska", 390, 2));
+catalogo.push(new producto(26,"Gorro", "Good", 650, 2));
+catalogo.push(new producto(27,"Gorro", "Miche", 920, 2));
+catalogo.push(new producto(28,"Gorro", "Rainbow", 900, 2));
+catalogo.push(new producto(29,"Remera", "NoProblem", 1200, 2));
+catalogo.push(new producto(30,"Remera", "FungiR", 1900, 2));
+catalogo.push(new producto(31,"Remera", "BatikSilver", 1850, 2));
+catalogo.push(new producto(32,"Remera", "BatikColor", 1800, 2));
+catalogo.push(new producto(33,"Accesorio", "ArosChapa", 350, 7));
+catalogo.push(new producto(34,"Accesorio", "CollarMari", 700, 5));
+catalogo.push(new producto(35,"Accesorio", "CollarPant", 600, 4));
+catalogo.push(new producto(36,"Piluso", "Beanie", 800, 2));
+catalogo.push(new producto(37,"Piluso", "PilusoNegro", 600, 2));
+catalogo.push(new producto(38,"Piluso", "PilusoNeon", 600, 2));
+catalogo.push(new producto(39,"Piluso", "PilusoBatik", 750, 7));
+catalogo.push(new producto(40,"Piluso", "PilusoNaranja", 600, 5));
+catalogo.push(new producto(41,"Piluso", "PilusoBlanco", 590, 4));
+//guardar el array en formato JSON
 const guardarLocal=(clave,valor)=>{localStorage.setItem(clave,valor)};
 
 guardarLocal("listaPrpductos",JSON.stringify(catalogo));
@@ -126,4 +123,16 @@ function elejirfiltro() {
 }
 
 
-elejirfiltro()
+elejirfiltro();
+function Validar(){
+let usuario=document.getElementById("usuario");
+let passw=document.getElementById("pass");
+if(usuario.value=="nico"&& passw.value=="1234"){
+    let parrafo=document.createElement("p");
+    parrafo.innerHTML="Bienvenido a Limo Tienda";
+    let mensaje=document.getElementById("mensaje");
+    mensaje.appendChild(parrafo);
+}else{
+    console.log("Error en los datos")
+}
+}
