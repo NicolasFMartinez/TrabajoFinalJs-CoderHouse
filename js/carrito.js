0c00a2bc82ed9713625dd9fd4fe04b74c3e229c5
Nicolas Martinez <nicolasfmartinez@hotmail.com>
2021-11-17 21:27:30 -0300

    se agrego la api de google maps y se agrego iconos de mail y cel

--- js/carrito.js
+++ js/carrito.js
@@ -132,4 +132,18 @@ function limpiarHtml() {
     while (contenedorCarrito.firstChild) {
         contenedorCarrito.removeChild(contenedorCarrito.firstChild)
     }
+};
+
+// map
+
+function iniciarMap(){
+    var coord = {lat:-34.5841939 ,lng: -58.4656398};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 15,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
 }
